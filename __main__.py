f6bc43f36da6633f0fecd81c71336b5d2415d3b3
saihate <workmalynych@gmail.com>
2026-07-29 15:48:09 +0300

    chore: clean up repo root, move __main__.py to package and remove devcontainer

--- __main__.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.dirname(__file__))
-
-from cli import main  # noqa: E402
-
-sys.exit(main())
---
+++ risk_calc/__main__.py
@@ -0,0 +1,4 @@
+from risk_calc import main
+
+if __name__ == "__main__":
+    main()
